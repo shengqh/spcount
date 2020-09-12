b4f9e438b896200410cbe025e86dc7c00f4724c4
Quanhu Sheng <shengqh@gmail.com>
2020-09-11 23:57:36 -0500

    move some functions to class

--- spcount/count_util.py
+++ spcount/count_util.py
@@ -7,32 +7,9 @@ import math
 import subprocess
 from _ctypes import ArgumentError
 
-def readFileMap(fileName):
-  result = {}
-  with open(fileName, "rt") as fin:
-    #fin.readline()
-    for line in fin:
-      parts = line.rstrip().split('\t')
-      result[parts[1]] = parts[0]
-  return(result)
-
-class BowtieCountItem(object):
-  def __init__(self, queryName, strand, chromosome, zeroBasedOffset, category, sample, count=1):
-    self.QueryName = queryName
-    self.Strand = strand
-    self.Chromosome = chromosome
-    self.ZeroBasedOffset = zeroBasedOffset
-    self.Category = category
-    self.Sample = sample
-    self.Count = count
-    self.Sequence = ''
-
-class CategoryEntry(object):
-  def __init__(self, name):
-    self.Name = name
-    self.TotalCount = 0
-    self.Items = []
-    self.QueryNames = set()
+from .CategoryEntry import CategoryEntry
+from .common_util import readFileMap
+from .BowtieCountItem import BowtieCountItem, readBowtieTextFile, getQueryMap, getCategoryMap, getCategoryUniqueCountMap, assignCount, getCategoryCountMap
 
 def removeSubset(logger, catMap):
   catItems = [v for v in catMap.values()]
@@ -77,45 +54,6 @@ def fillQueryCount(logger, queryMap, countFile):
         item.Count = count
         item.Sequence = sequence  
 
-def getQueryMap(bowtieItems):
-  result = {}
-  for item in bowtieItems:
-    result.setdefault(item.QueryName, []).append(item)
-  return(result)
-
-def getCategoryMap(bowtieItems):
-  result = {}
-  for item in bowtieItems:
-    result.setdefault(item.Category, []).append(item)
-  return(result)
-
-def getCategoryUniqueCountMap(bowtieItems):
-  result = {}
-  for item in bowtieItems:
-    if item.Category not in result:
-      result[item.Category] = 1
-    else:
-      result[item.Category] = result[item.Category] + 1
-  return(result)
-
-def assignCount(queryMap, bowtieItems):
-  catCountMap = getCategoryUniqueCountMap(bowtieItems)
-  for items in queryMap.values():
-    catCounts = [catCountMap[item.Category] for item in items]
-    totalCount = sum(catCounts)
-    proportion = [cc / totalCount for cc in catCounts]
-    for idx in range(0, len(proportion)):
-      items[idx].Count = items[idx].Count * proportion[idx]
-
-def getCategoryCountMap(bowtieItems):
-  result = {}
-  for item in bowtieItems:
-    #print("%s\t%d" %(item.Category, item.Count))
-    if item.Category not in result:
-      result[item.Category] = 0
-    result[item.Category] = result[item.Category] + item.Count
-  return(result)
-
 def count(logger, inputListFile, outputFile, countListFile):
   logger.info("Start count ...")
 
@@ -136,13 +74,8 @@ def count(logger, inputListFile, outputFile, countListFile):
 
     logger.info("Processing %s ..." % bowtieFile)
 
-    bowtieItems = []
-    with open(bowtieFile, "rt") as fin:
-      for line in fin:
-        parts = line.rstrip().split('\t')
-        queryName = parts[0]
-        bowtieItems.append(BowtieCountItem(queryName, parts[1], parts[2], parts[3], parts[4], sample))
-
+    bowtieItems = readBowtieTextFile(bowtieFile, sample)
+     
     queryMap = getQueryMap(bowtieItems)
 
     if sample in countFileMap:
