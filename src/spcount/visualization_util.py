25ef9c38323a7e374827a3940fc86647ae309ca0
Quanhu Sheng <shengqh@gmail.com>
2022-07-19 23:46:39 -0500

    v0.0.12

--- src/spcount/visualization_util.py
+++ src/spcount/visualization_util.py
@@ -1,6 +1,21 @@
 import pandas as pd
 import subprocess
 import logging
+import os
+
+def read_taxonomy_name_map(taxonomy_file):
+  name_map = {}
+  with open(taxonomy_file, "rt") as fin:
+    for line in fin:
+      parts = line.rstrip().split('\t')
+      name_map[parts[4]] = {
+        "id":int(parts[0]),
+        "rank_level": int(parts[1]),
+        "parent_id": int(parts[2]),
+        "rank": parts[3],
+        "name": parts[4]
+      }
+  return(name_map)
 
 def draw_krona(logger, treeFile, taxonomyFolder, outputPrefix):
   tree_data = pd.read_csv(treeFile, sep="\t")
@@ -20,14 +35,26 @@ def draw_krona(logger, treeFile, taxonomyFolder, outputPrefix):
     logger.info(" ".join(args))
     subprocess.call(args)
 
+def get_taxonomy_id(name_map, row):  
+  return (name_map[row['Feature']]['id'])
+
+def get_rank(name_map, row):  
+  return (name_map[row['Feature']]['rank'])
+
 def krona(logger, treeFile, groupFile, taxonomyFolder, outputPrefix):
+  tree_data = pd.read_csv(treeFile, sep="\t")
+  if tree_data.columns[1] != "TaxonomyId":
+    name_map = read_taxonomy_name_map(os.path.join(taxonomyFolder, "taxonomy.tab"))
+    tree_data.insert(loc=1, column="TaxonomyId", value=tree_data.apply(lambda row: get_taxonomy_id(name_map, row), axis=1))
+    tree_data.insert(loc=2, column="Rank", value=tree_data.apply(lambda row: get_rank(name_map, row), axis=1))
+    treeFile=outputPrefix + ".tree.count"
+    tree_data.to_csv(treeFile, sep="\t", index=False)
+
   logger.info("Start sample krona ...")
   
   draw_krona(logger, treeFile, taxonomyFolder, outputPrefix)
 
   logger.info("Start group krona ...")
-
-  tree_data = pd.read_csv(treeFile, sep="\t")
 
   logger.info(f"Read group info {groupFile} ...")
   groups_df=pd.read_csv(groupFile, sep="\t", header=None)
@@ -42,7 +69,7 @@ def krona(logger, treeFile, groupFile, taxonomyFolder, outputPrefix):
     gsamples = gdf['sample'].tolist()
     group_data[gname] = tree_data[gsamples].sum(axis=1)
 
-  group_file = outputPrefix + ".group.txt";
+  group_file = outputPrefix + ".group.txt"
   group_data.to_csv(group_file, sep="\t", index=None)
 
   draw_krona(logger, group_file, taxonomyFolder, outputPrefix)
